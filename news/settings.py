adca112546c5f27896e4bba162d832742079dffd
rokari-19 <oshomahiyelobu1@gmail.com>
2025-03-13 23:16:01 +0100

    changed settings.py

--- news/settings.py
+++ news/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m%i^29do$$uftudes2ccpgknq2xj$k))+2%q9em73q@apbafvx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 CORS_ALLOW_ALL_ORIGINS = True
